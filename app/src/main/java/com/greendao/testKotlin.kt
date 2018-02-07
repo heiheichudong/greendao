49daf74c765e56bd085f89775d742fe272052c88
zl <zhenglei@5.cn>
2018-02-07 18:17:52 +0800

    frist

---
+++ app/src/main/java/com/greendao/testKotlin.kt
@@ -0,0 +1,9 @@
+package com.greendao
+
+/**
+ * @author zl
+ * @data 2018/2/7
+ */
+fun testCall():String{
+    return "调用成功";
+}
---
+++ greentest/src/main/java/com/greentest/Utils.kt
@@ -0,0 +1,9 @@
+package com.greentest
+
+/**
+ * @author zl
+ * @data 2018/2/7
+ */
+fun testAppCall():String{
+    return "调用成功"
+}
